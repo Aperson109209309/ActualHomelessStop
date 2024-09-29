20361d9b8fd47fb411c3a28f628bc9fbc9ba5b55
Eddie He <ehbs20@outlook.com>
2024-09-29 13:40:07 -0700

    made email n/a applicable

--- ActualHomelessStop/app/models.py
+++ ActualHomelessStop/app/models.py
@@ -9,7 +9,7 @@ class Nonprofit(models.Model):
     # Define fields for the Donor model
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=500, blank=True, null=True)
-    email = models.EmailField(unique=False)
+    email = models.EmailField(unique=False, blank=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
